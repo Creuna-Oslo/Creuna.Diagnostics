926314ced0972ef53923ff85021bed5e02fea564
Vladimir Levchuk <vladimir.levchuk@creuna.no>
2018-03-19 19:51:34 +0200

    re-installed nuget packages, added a common version for all but TelemetryFiltering; built v0.8.2

--- EPiLog/Properties/AssemblyInfo.cs
+++ EPiLog/Properties/AssemblyInfo.cs
@@ -1,37 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EPiLog")]
-[assembly: AssemblyDescription("EPiServer Serilog integration")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Vladimir Levchuk, Creuna Kharkiv")]
 [assembly: AssemblyProduct("EPiLog")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("EPiServer Serilog integration")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("10ad21d9-1a0b-443b-b2c3-4852a078a9f2")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyInformationalVersion("0.8.0")]
-[assembly: AssemblyFileVersion("0.8.0")]
